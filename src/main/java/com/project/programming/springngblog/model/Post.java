b754d687250f3aa67d0e24d682350ea5ce1d6ae5
onkar-sopho <onkar21.rai@gmail.com>
2022-02-02 20:46:09 +0530

    Added getters and setters for Post entity

--- src/main/java/com/project/programming/springngblog/model/Post.java
+++ src/main/java/com/project/programming/springngblog/model/Post.java
@@ -24,4 +24,52 @@ public class Post {
     @Column
     @NotBlank
     private String username;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreatedOn() {
+        return this.createdOn;
+    }
+
+    public void setCreatedOn(Instant createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Instant getUpdatedOn() {
+        return this.updatedOn;
+    }
+
+    public void setUpdatedOn(Instant updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
