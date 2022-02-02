8c8ddefb2a7ba90ae4567b0e2b8c1c72ad0b75dc
onkar-sopho <onkar21.rai@gmail.com>
2022-02-02 20:35:59 +0530

    Added required annotations to Post entity

--- src/main/java/com/project/programming/springngblog/model/Post.java
+++ src/main/java/com/project/programming/springngblog/model/Post.java
@@ -1,12 +1,27 @@
 package com.project.programming.springngblog.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.time.Instant;
 
+@Entity
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
+    @Column
     private String title;
+    @Lob
+    @NotEmpty
+    @Column
     private String content;
+    @Column
     private Instant createdOn;
+    @Column
     private Instant updatedOn;
+    @Column
+    @NotBlank
     private String username;
 }
