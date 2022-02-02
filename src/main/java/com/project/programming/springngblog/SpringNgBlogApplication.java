324fcd5f7808179d420249a09ea45d23c0fb35e5
onkar-sopho <onkar21.rai@gmail.com>
2022-02-02 16:53:42 +0530

    Initial commit

---
+++ src/main/java/com/project/programming/springngblog/SpringNgBlogApplication.java
@@ -0,0 +1,13 @@
+package com.project.programming.springngblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNgBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNgBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/programming/springngblog/SpringNgBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.programming.springngblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNgBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
