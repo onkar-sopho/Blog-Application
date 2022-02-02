56579084fe41af1cef5af78750cbf5fd3383e7f4
onkar-sopho <onkar21.rai@gmail.com>
2022-02-02 19:03:11 +0530

    Created the basic domain entities - post and user

---
+++ src/main/java/com/project/programming/springngblog/model/Post.java
@@ -0,0 +1,12 @@
+package com.project.programming.springngblog.model;
+
+import java.time.Instant;
+
+public class Post {
+    private Long id;
+    private String title;
+    private String content;
+    private Instant createdOn;
+    private Instant updatedOn;
+    private String username;
+}
---
+++ src/main/java/com/project/programming/springngblog/model/User.java
@@ -0,0 +1,8 @@
+package com.project.programming.springngblog.model;
+
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+}
